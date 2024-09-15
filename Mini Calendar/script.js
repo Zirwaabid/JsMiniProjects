ca546fc1b8f18fbbf738447581ff716a6a807c0c
Zirwa Abid <zirwaabid112@gmail.com>
2024-09-15 12:21:02 -0700

    calendar

---
+++ Mini Calendar/script.js
@@ -0,0 +1,19 @@
+const MonthNameE1 = document.getElementById("month-name");
+const DayNameE1 = document.getElementById("day-name");
+const DayNumE1 = document.getElementById("day-number");
+const YearNameE1 = document.getElementById("year");
+
+const date = new Date();
+MonthNameE1.innerText = date.toLocaleDateString("en", {
+    month: "long"
+});
+DayNameE1.innerText = date.toLocaleDateString("en", {
+    weekday: "long"
+});
+
+DayNameE1.innerText = date.toLocaleDateString("en", {
+    weekday: "long"
+});
+
+DayNumE1.innerText = date.getDate();
+YearNameE1.innerText = date.getFullYear();
